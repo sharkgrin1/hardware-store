150150c62145c7644214cc4b049607fbf032b0a3
sharkgrin1 <mr.kostell@mail.ru>
2023-02-12 20:38:19 +1300

    fixed(store-service): deleted UserUnmapper.java

--- store-service/src/main/java/store/repositories/jooq/UserRepositoryImpl.java
+++ store-service/src/main/java/store/repositories/jooq/UserRepositoryImpl.java
@@ -2,10 +2,9 @@ package store.repositories.jooq;
 
 import org.jooq.DSLContext;
 import org.springframework.stereotype.Repository;
-import store.repositories.domain.User;
 import store.repositories.UserRepository;
 import store.repositories.converters.UserMapper;
-import store.repositories.converters.UserUnmapper;
+import store.repositories.domain.User;
 
 import java.util.Optional;
 
@@ -14,12 +13,10 @@ import static store.jooq.tables.Users.USERS;
 @Repository
 public class UserRepositoryImpl implements UserRepository {
     private final DSLContext dsl;
-    private final UserUnmapper unmapper;
     private final UserMapper mapper;
 
-    public UserRepositoryImpl(DSLContext dsl, UserUnmapper unmapper, UserMapper mapper) {
+    public UserRepositoryImpl(DSLContext dsl, UserMapper mapper) {
         this.dsl = dsl;
-        this.unmapper = unmapper;
         this.mapper = mapper;
     }
 
