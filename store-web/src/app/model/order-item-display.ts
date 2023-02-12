36826a04bdd1d6e52a531530adf9aa4faf5a9414
sharkgrin1 <mr.kostell@mail.ru>
2023-02-13 11:58:26 +1300

    front:fixed: added fields bought and paid for Product, added field productPrice for OrderItemDisplay

--- store-web/src/app/model/order-item-display.ts
+++ store-web/src/app/model/order-item-display.ts
@@ -2,6 +2,7 @@ export class OrderItemDisplay {
   constructor(public id: number,
               public userId: number,
               public productName: string,
+              public productPrice: number,
               public total: number,
               public quantity: number) {
   }
--- store-web/src/app/model/product.ts
+++ store-web/src/app/model/product.ts
@@ -1,8 +1,11 @@
 export class Product {
-  public bought?:boolean;
+  public bought?: boolean;
+  public paid?:boolean;
+
   constructor(readonly id: number,
               public name: string,
               public price: number,
-              public quantity: number) {
+              public quantity: number,
+              public hidden: boolean) {
   }
 }
