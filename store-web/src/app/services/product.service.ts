0498d3c649ad62b404c562bd59911b48e120cb30
sharkgrin1 <mr.kostell@mail.ru>
2023-02-12 23:06:24 +1300

    front:fixed: added create(), delete() and update() in ProductService

--- store-web/src/app/services/product.service.ts
+++ store-web/src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Product} from "../model/product";
 import {User} from "../model/user";
+import {AppComponent} from "../app.component";
 
 @Injectable()
 export class ProductService {
@@ -10,8 +11,38 @@ export class ProductService {
   }
 
   public findAll(): Observable<Product[]> {
-    const user: User = JSON.parse(localStorage.getItem('user'));
+    const user: User = AppComponent.getUser();
     return this.http.get<Product[]>(`/api/products`, {
+      params: {
+        username: user.username,
+        token: user.token
+      }
+    });
+  }
+
+  public create(product: Product): Observable<Product> {
+    const user: User = AppComponent.getUser();
+    return this.http.post<Product>(`/api/products`, product, {
+      params: {
+        username: user.username,
+        token: user.token
+      }
+    });
+  }
+
+  public delete(id: number): Observable<void> {
+    const user = AppComponent.getUser();
+    return this.http.delete<void>(`/api/products/${id}`, {
+      params: {
+        username: user.username,
+        token: user.token
+      }
+    });
+  }
+
+  update(product: Product): Observable<Product> {
+    const user: User = AppComponent.getUser();
+    return this.http.put<Product>(`/api/products`, product, {
       params: {
         username: user.username,
         token: user.token
